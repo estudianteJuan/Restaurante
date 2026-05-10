876f5fe8edc51d1a5aa63ff71371c6468536037f
estudianteJuan <juangarcia709@tecnologicadeloriente.edu.co>
2026-05-10 00:04:19 -0500

    Actualizacion de BD3

--- index.js
+++ index.js
@@ -239,10 +239,4 @@ const db = mysql.createPool({
     queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err);
-        return;
-    }
-    console.log('Conectado a la base de datos MySQL.');
-});
+
